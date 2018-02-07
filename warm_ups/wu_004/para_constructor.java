ab3b8970f6254e7ac81f95a52c0915fca7a2e1ae
Alan Cugler <alan-cugler@users.noreply.github.com>
2018-02-07 15:00:11 -0600

    add a parameterized constructor example

---
+++ warm_ups/wu_004/para_constructor.java
@@ -0,0 +1,10 @@
+// Creating a parameterized constructor
+
+class test{
+
+    test(int i){
+        System.out.println("parameterized constructor "+i);}
+
+    public static void main(String args[]){
+        test new_object1=new test(1234);}
+}
