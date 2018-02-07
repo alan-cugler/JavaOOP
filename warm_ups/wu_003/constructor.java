d751b18b081259077ebc7f7c24c2287151cad896
Alan Cugler <alan-cugler@users.noreply.github.com>
2018-02-07 14:28:37 -0600

    default constructor example added in warm_ups/

---
+++ warm_ups/wu_003/constructor.java
@@ -0,0 +1,20 @@
+// Creating a constructor
+
+class test{
+
+    test(){
+        System.out.println("default constructor");}
+
+    public static void main(String args[]){
+        test new_object=new test();}
+}
+
+////////////////////////////////////////////////////
+/*                    NOTES                       */
+////////////////////////////////////////////////////
+/* Constructor gets the same name as its class.   */
+/* No return type.                                */
+/* No void parameterization.                      */
+/* Default constructor evoked when class object   */
+/* is declared.                                   */
+////////////////////////////////////////////////////
