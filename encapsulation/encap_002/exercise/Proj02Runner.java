c814d735047971383d7137824b98586fd61a73a8
Alan Cugler <alan.cugler101@gmail.com>
2018-02-21 20:42:10 -0600

    Asg 2 is done

--- encapsulation/encap_002/exercise/Proj02Runner.java
+++ encapsulation/encap_002/exercise/Proj02Runner.java
@@ -1,6 +1,71 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.io.IOException;
 
-class Proj92Runner{
+class Proj02Runner{
 
+    Picture picture = new Picture("Proj02.jpg");
+    private Turtle Redds;
+
+    private Pixel pix;
+    public int x, y, xCor, yCor;
     
+    public int[] latitude = new int[394];
+    public int[] longitude = new int[295];
+    
+    public Proj02Runner(){
+        System.out.println("Alan Cugler.");}
 
+
+    //Setting run function
+    public void run(){
+
+        //Print on picture
+        Redds = new Turtle(394,295,picture);
+                           //picture.getWidth(),picture.getHeight()/2,picture);
+
+        picture.addMessage("I certify that this program is my own work", 10, 20);
+        picture.addMessage("and is not the work of others. I agree not", 10, 38);
+        picture.addMessage("to share my solution with others.", 10, 56);
+        picture.addMessage("Alan Cugler.", 10, 74);
+
+
+        for(int x = 0; x < latitude.length; x++){
+            for(int y = 0; y < longitude.length; y++){
+                pix = picture.getPixel(x,y);
+                //System.out.println(-0.397959*x+77);
+                if (y < (-0.397959*x-77)*-1){
+                    pix.setRed(255 - pix.getRed());
+                    pix.setGreen(255 - pix.getGreen());
+                    pix.setBlue(127);
+                }}}
+
+        Redds.hide();
+        Redds.setPenWidth(8);
+        Redds.setPenColor(Color.RED);
+        Redds.penUp();
+        Redds.moveTo(0,55*picture.getHeight()/200);
+        Redds.penDown();
+        Redds.moveTo(picture.getWidth(),161*picture.getHeight()/200);
+        Redds.penUp();
+        
+        //Pixel[] pixels = picture.getPixels();
+
+        //for(Pixel pix:pixels ){
+        //  pix.setRed(255 - pix.getRed());
+        //   pix.setGreen(255 - pix.getGreen());
+        //  pix.setBlue(127);}
+        //end for loop
+
+        
+        
+        //displaying the modified picture
+        picture.explore();
+    }
+
+        //
+        public Picture getPicture(){
+            return picture;}
 }
