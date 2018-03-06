890c95534f361861d4c7d5c28433840497c33c93
Alan Cugler <alan.cugler101@gmail.com>
2018-03-06 15:32:42 -0600

    Encapsulation Project 3 completed

--- encapsulation/encap_003/exercise/Proj03Runner.java
+++ encapsulation/encap_003/exercise/Proj03Runner.java
@@ -10,53 +10,80 @@ class Proj03Runner{
     private int width = picture.getWidth();
     private int height = picture.getHeight();
     private int[] latitude, longitude;
-    private double xOff, yOff, xScale, yScale, yVal, xVal, greenScale, redScale, blueScale; 
+    private double xScale, yScale, yVal, xVal;
+    private double greenScale, redScale, blueScale, curveLine; 
 
-    Picture getPicture(){
-        return picture;
-    }
     
-    void run(){
+    //Constructor
+    public Proj03Runner(){
         System.out.println("Alan Cugler");
+    }
+
+
+    //Main function
+    void run(){
+    
+        //Draws the parabola & color shading below it
+        parabolaColoringLines();
+        
+        //Text added to picture
+        pictureMessage();
+
+        //Publishing modified picture
+        picture.explore();
+    }
+
+    
+    private void parabolaColoringLines(){
 
         xScale = 0.9 * (double)height/2;
 
         for (xCor = 0; xCor < width; ++xCor){
             for (yCor = 0; yCor < height; ++yCor) {
-                
-                Pixel pixel = picture.getPixel(xCor, yCor);
-                red = pixel.getRed();
-                green = pixel.getGreen();
-                blue = pixel.getBlue();
-                
-                double red_pixel_change = (double)xCor / (double)width;
-                double green_pixel_change = (double)(width-xCor) / (double)width;
-                
-                double cubic = function((double)(xCor-width/2) / (double)(width/2));
-                double curveLine = xScale * cubic + (double)(height/4);
+
+                pix = picture.getPixel(xCor, yCor);
+                red = pix.getRed();
+                green = pix.getGreen();
+
+                greenScale = (double)(width - xCor)/width;
+                redScale = (double)(xCor)/width;
+
+                yVal = cubic((double)(xCor-width/2) / (double)(width/2));
+                curveLine = xScale * yVal + (double)(height/4);
+
 
                 //drawing line
                 if ((double)yCor < curveLine + 5.0 && (double)yCor > curveLine - 5.0) {
-                    pixel.setColor(Color.RED);
-                }
+                    pix.setColor(Color.RED);}
 
                 //shading below the line
                 else if ((double)yCor > curveLine) {
-                    pixel.setRed((int)((double)red * red_pixel_change));
-                    pixel.setGreen((int)((double)green * green_pixel_change));
-                    pixel.setBlue((int)((double)blue));
-                }}}
+                    pix.setGreen((int)(green * greenScale));
+                    pix.setRed((int)(red * redScale));
+                }
+            }
+        }
+    }
+
+    
+    //Pictue message function
+    private void pictureMessage(){
 
         picture.addMessage("I certify that this program is my own work", 10, 20);
         picture.addMessage("and is not the work of others. I agree not", 10, 38);
         picture.addMessage("to share my solution with others.", 10, 56);
         picture.addMessage("Alan Cugler", 10, 74);
-        picture.explore();
     }
-    
-    
 
-    double function(double xVal){
-        double yVal = xVal*xVal*xVal;
+    
+    //Cubic parabola function
+    private double cubic(double xVal){
+
+        yVal = xVal*xVal*xVal;
         return yVal;
-    }}
+    }
+
+
+    //Returns picture to calling program
+    public Picture getPicture(){ return picture; }
+}
